b063aedc32a88940ab604b527a67760b82c939d4
arshsaini15 <arshdeepp015@gmail.com>
2025-12-29 17:16:43 +0530

    Modified Controller

---
+++ src/main/java/com/notification/eventdriven/controller/NotificationController.java
@@ -0,0 +1,54 @@
+package com.notification.eventdriven.controller;
+
+import com.notification.eventdriven.dto.request.UpdateStatusRequest;
+import com.notification.eventdriven.dto.response.NotificationResponse;
+import com.notification.eventdriven.enums.NotificationStatus;
+import com.notification.eventdriven.exceptions.NotificationNotFoundException;
+import com.notification.eventdriven.mapper.NotificationMapper;
+import com.notification.eventdriven.model.Notification;
+import com.notification.eventdriven.service.NotificationService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/notifications")
+public class NotificationController {
+
+    private final NotificationService notificationService;
+
+    public NotificationController(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    @GetMapping("/event/{eventId}")
+    public Notification getByEventId(@PathVariable Long eventId) {
+        return notificationService.getByEventId(eventId)
+                .orElseThrow(() ->
+                        new NotificationNotFoundException(
+                                "Notification with eventId " + eventId + " not found"
+                        )
+                );
+    }
+
+    @PutMapping("/{notificationId}/status")
+    public NotificationResponse updateStatus(
+            @PathVariable Long notificationId,
+            @RequestBody @Valid UpdateStatusRequest request
+    ) {
+        return NotificationMapper.toDto(
+                notificationService.updateStatus(notificationId, request.getStatus())
+        );
+    }
+
+
+    @GetMapping
+    public Page<Notification> getNotifications(
+            @RequestParam(required = false) NotificationStatus status,
+            Pageable pageable
+    ) {
+        return notificationService.getNotifications(status, pageable);
+    }
+}
