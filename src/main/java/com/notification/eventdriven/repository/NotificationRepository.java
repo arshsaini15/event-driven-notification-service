ccd739d6eee9602c22ae78e756c9eec4104d2300
arshsaini15 <arshdeepp015@gmail.com>
2025-12-29 16:58:46 +0530

    Notification Repository(paging implementation)

--- src/main/java/com/notification/eventdriven/repository/NotificationRepository.java
+++ src/main/java/com/notification/eventdriven/repository/NotificationRepository.java
@@ -2,14 +2,14 @@ package com.notification.eventdriven.repository;
 
 import com.notification.eventdriven.enums.NotificationStatus;
 import com.notification.eventdriven.model.Notification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 import java.util.Optional;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
-    Optional<Notification> findByEventId(String eventId);
+    Optional<Notification> findByEventId(Long eventId);
 
-    List<Notification> findByStatus(NotificationStatus status);
+    Page<Notification> findByStatus(NotificationStatus status, Pageable pageable);
 }
