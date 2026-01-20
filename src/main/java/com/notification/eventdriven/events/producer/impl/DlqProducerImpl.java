00cbef5f10010c23c3ce399125f83e9a59726857
arshsaini15 <arshdeepp015@gmail.com>
2026-01-20 20:47:03 +0530

    DLQ prodcuer Implementation for producing DLQ events

--- src/main/java/com/notification/eventdriven/events/producer/impl/DlqProducerImpl.java
+++ src/main/java/com/notification/eventdriven/events/producer/impl/DlqProducerImpl.java
@@ -1,9 +1,11 @@
 package com.notification.eventdriven.events.producer.impl;
 
 import com.notification.eventdriven.events.NotificationEvent;
+import com.notification.eventdriven.events.RetryHeaders;
 import com.notification.eventdriven.events.producer.DlqProducer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -20,13 +22,17 @@ public class DlqProducerImpl implements DlqProducer {
 
     @Override
     public void publish(NotificationEvent event, Exception cause) {
+
         log.error(
-                "Publishing event {} to DLQ [{}] due to {}",
-                event.getEventId(),
-                DLQ_TOPIC,
-                cause.getMessage()
+                "Sending event {} to DLQ after retries exhausted",
+                event.getEventId()
         );
 
-        kafkaTemplate.send(DLQ_TOPIC, event.getEventId(), event);
+        kafkaTemplate.send(
+                MessageBuilder
+                        .withPayload(event)
+                        .setHeader(RetryHeaders.ERROR_REASON, cause.getMessage())
+                        .build()
+        );
     }
 }
