5271ad6339ffbfdeb45f857b5e7f6fee182a980a
arshsaini15 <arshdeepp015@gmail.com>
2026-01-20 20:48:32 +0530

    Retry notification markSent or throw error

--- src/main/java/com/notification/eventdriven/scheduler/NotificationRetryJob.java
+++ src/main/java/com/notification/eventdriven/scheduler/NotificationRetryJob.java
@@ -2,7 +2,6 @@ package com.notification.eventdriven.scheduler;
 
 import com.notification.eventdriven.model.Notification;
 import com.notification.eventdriven.service.NotificationSender;
-import com.notification.eventdriven.service.NotificationService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
