4098b6dfaa3762a21e2e9d722d6d1e2e2ad78bc3
arshsaini15 <arshdeepp015@gmail.com>
2026-01-21 17:07:01 +0530

    Dlq Replay Service Impl

---
+++ src/main/java/com/notification/eventdriven/service/DlqReplayService.java
@@ -0,0 +1,52 @@
+package com.notification.eventdriven.service;
+
+import com.notification.eventdriven.events.NotificationEvent;
+import com.notification.eventdriven.events.producer.DlqReplayProducer;
+import com.notification.eventdriven.model.Notification;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class DlqReplayService {
+
+    private final NotificationService notificationService;
+    private final DlqReplayProducer replayProducer;
+
+    public DlqReplayService(
+            NotificationService notificationService,
+            DlqReplayProducer replayProducer
+    ) {
+        this.notificationService = notificationService;
+        this.replayProducer = replayProducer;
+    }
+
+    public void replay(String eventId) {
+
+        Notification notification =
+                notificationService.getByEventId(eventId);
+
+        if (notification.getStatus().name().equals("SENT")) {
+            log.info(
+                    "Notification already SENT. Replay skipped → eventId={}",
+                    eventId
+            );
+            return;
+        }
+
+        if (!notification.getStatus().name().equals("DEAD")) {
+            throw new IllegalStateException(
+                    "Only DEAD notifications can be replayed"
+            );
+        }
+
+        NotificationEvent event = new NotificationEvent(
+                eventId,
+                notification.getMessage()
+        );
+
+        replayProducer.replay(event);
+    }
+}
